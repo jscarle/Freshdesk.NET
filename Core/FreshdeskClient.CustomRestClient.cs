f0970717583c67a7e46912d27ead26fba162fddb
jscarle <29762210+jscarle@users.noreply.github.com>
2019-07-26 18:05:39 -0400

    Propogated the CancellationToken

--- Core/FreshdeskClient.CustomRestClient.cs
+++ Core/FreshdeskClient.CustomRestClient.cs
@@ -34,13 +34,7 @@ namespace Freshdesk
             }
 
             public new Response Execute(IRestRequest request)
-            {
-                IRestResponse response = base.Execute(request);
-                if (!IsSuccessfulRequest(response.StatusCode))
-                    return new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings));
-
-                return new Response(response);
-            }
+                => ExecuteTaskAsync(request).WaitAndUnwrapException<Response>();
 
             public new async Task<Response> ExecuteTaskAsync(IRestRequest request, CancellationToken cancellationToken = default)
             {
@@ -51,51 +45,8 @@ namespace Freshdesk
                 return new Response(response);
             }
 
-            public new(Response, T) Execute<T>(IRestRequest request) where T : class, new()
-            {
-                if (typeof(T).IsGenericType && typeof(T).GetGenericTypeDefinition() == typeof(List<>))
-                {
-                    request.Resource = AppendParametertoUri(request.Resource, "per_page", PAGESIZE_MAXIMUM.ToString());
-
-                    Type genericType = typeof(T).GetGenericTypeDefinition().MakeGenericType(typeof(T).GetGenericArguments().First());
-                    IList pagedData = (IList)Activator.CreateInstance(genericType);
-
-                    IRestResponse response = base.Execute(request);
-                    if (!IsSuccessfulRequest(response.StatusCode))
-                        return (new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings)), null);
-
-                    foreach (object data in (IEnumerable)JsonConvert.DeserializeObject<T>(response.Content, jsonSerializerSettings))
-                        pagedData.Add(data);
-
-                    string nextPageUri = GetNextPageUri(response.Headers);
-                    int nextPage = ExtractPageNumber(nextPageUri);
-
-                    while (!String.IsNullOrWhiteSpace(nextPageUri) && nextPage <= PAGES_RECOMMENDEDLIMIT)
-                    {
-                        request.Resource = nextPageUri;
-
-                        response = base.Execute(request);
-                        if (!IsSuccessfulRequest(response.StatusCode))
-                            return (new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings)), null);
-
-                        foreach (object data in (IEnumerable)JsonConvert.DeserializeObject<T>(response.Content, jsonSerializerSettings))
-                            pagedData.Add(data);
-
-                        nextPageUri = GetNextPageUri(response.Headers);
-                        nextPage = ExtractPageNumber(nextPageUri);
-                    }
-
-                    return (new Response(response), (T)pagedData);
-                }
-                else
-                {
-                    IRestResponse response = base.Execute(request);
-                    if (!IsSuccessfulRequest(response.StatusCode))
-                        return (new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings)), null);
-
-                    return (new Response(response), JsonConvert.DeserializeObject<T>(response.Content, jsonSerializerSettings));
-                }
-            }
+            public new (Response, T) Execute<T>(IRestRequest request) where T : class, new()
+                => ExecuteTaskAsync<T>(request).WaitAndUnwrapException<(Response, T)>();
 
             public new async Task<(Response, T)> ExecuteTaskAsync<T>(IRestRequest request, CancellationToken cancellationToken = default) where T : class, new()
             {
@@ -118,6 +69,9 @@ namespace Freshdesk
 
                     while (!String.IsNullOrWhiteSpace(nextPageUri) && nextPage <= PAGES_RECOMMENDEDLIMIT)
                     {
+                        if (cancellationToken.IsCancellationRequested)
+                            break;
+
                         request.Resource = nextPageUri;
 
                         response = await base.ExecuteTaskAsync(request, cancellationToken).ConfigureAwait(false);
@@ -144,37 +98,7 @@ namespace Freshdesk
             }
 
             public (Response, T) ExecuteSearch<T>(IRestRequest request) where T : class, new()
-            {
-                request.Resource = AppendParametertoUri(request.Resource, "page", "1");
-
-                Type genericType = typeof(T).GetGenericTypeDefinition().MakeGenericType(typeof(T).GetGenericArguments().First());
-                IList pagedData = (IList)Activator.CreateInstance(genericType);
-
-                IRestResponse response = base.Execute(request);
-                if (!IsSuccessfulRequest(response.StatusCode))
-                    return (new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings)), null);
-
-                SearchResults<T> initialResults = JsonConvert.DeserializeObject<SearchResults<T>>(response.Content, jsonSerializerSettings);
-                foreach (object data in (IEnumerable)initialResults.Results)
-                    pagedData.Add(data);
-
-                int pages = (int)Math.Ceiling((decimal)initialResults.Total / (decimal)30);
-                if (pages > 10)
-                    pages = PAGES_FILTEREDLIMIT;
-
-                for (int page = 2; page <= pages; page++)
-                {
-                    response = base.Execute(request);
-                    if (!IsSuccessfulRequest(response.StatusCode))
-                        return (new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings)), null);
-
-                    SearchResults<T> additionnalResults = JsonConvert.DeserializeObject<SearchResults<T>>(response.Content, jsonSerializerSettings);
-                    foreach (object data in (IEnumerable)additionnalResults.Results)
-                        pagedData.Add(data);
-                }
-
-                return (new Response(response), (T)pagedData);
-            }
+                => ExecuteSearchTaskAsync<T>(request).WaitAndUnwrapException<(Response, T)>();
 
             public async Task<(Response, T)> ExecuteSearchTaskAsync<T>(IRestRequest request, CancellationToken cancellationToken = default) where T : class, new()
             {
@@ -197,7 +121,10 @@ namespace Freshdesk
 
                 for (int page = 2; page <= pages; page++)
                 {
-                    response = await base.ExecuteTaskAsync(request).ConfigureAwait(false);
+                    if (cancellationToken.IsCancellationRequested)
+                        break;
+
+                    response = await base.ExecuteTaskAsync(request, cancellationToken).ConfigureAwait(false);
                     if (!IsSuccessfulRequest(response.StatusCode))
                         return (new Response(response, JsonConvert.DeserializeObject<Error>(response.Content, jsonSerializerSettings)), null);
 
